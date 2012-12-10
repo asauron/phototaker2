8b8ec57bb35b2479e098116dac2372706292c964
Senthil Kumaran <senthil@uthcode.com>
2012-12-10 03:14:01 -0800

    .

--- src/com/example/phototaker2/MainActivity.java
+++ src/com/example/phototaker2/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.phototaker2;
 
+/*
+ * http://crw-cmu.googlecode.com/svn-history/r734/luis/PhotoIntentActivity/src/com/example/android/photobyintent/PhotoIntentActivity.java
+ */
+
+import java.io.File;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends Activity {
 	
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	/* Checks if external storage is available for read and write */
+	public boolean isExternalStorageWritable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+	
+	public File getAlbumStorageDir(String albumName) {
+	    // Get the directory for the user's public pictures directory. 
+	    File file = new File(Environment.getExternalStoragePublicDirectory(
+	            Environment.DIRECTORY_PICTURES), albumName);
+	    if (!file.mkdirs()) {
+	        Log.e("PhotoTaker1", "Directory not created");
+	    }
+	    return file;
+	}
 	
 
 	@Override
@@ -31,6 +59,14 @@ public class MainActivity extends Activity {
 		intent.putExtra(EXTRA_MESSAGE, message);
 		startActivity(intent);
 		
+	}
+	
+	public void showMaps(View view) {
+		
+		Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("google.navigation:q=My+Location"));
+		startActivity(intent);
+		
+	
 	}
 
 }
