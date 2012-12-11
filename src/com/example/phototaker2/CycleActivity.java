a3cb610fd88baaeec988f3d17e46a167c9da279b
Senthil Kumaran <senthil@uthcode.com>
2012-12-11 02:52:09 -0800

    onActivityResult

--- src/com/example/phototaker2/CycleActivity.java
+++ src/com/example/phototaker2/CycleActivity.java
@@ -104,7 +104,21 @@ public class CycleActivity extends Activity implements LocationListener {
 			Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 			Uri uriSavedImage = Uri.fromFile(new File(photoname));
 			takePictureIntent.putExtra("output", uriSavedImage);
-			startActivityForResult(takePictureIntent, RESULT_OK); /* What is RESULT_OK */			
+			startActivityForResult(takePictureIntent, RESULT_OK); /* What is RESULT_OK */	
+			
+		
 		}
+	  
+	  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		  if (requestCode == 1) {
+		       if(resultCode == RESULT_OK) {
+		   		Intent email = new Intent(Intent.ACTION_SEND);
+				email.setType("message/rfc822");
+				startActivity(Intent.createChooser(email, "Choose an Email client :"));
+		       }
 
+		  }
+	  }
+	  
 } 
+
