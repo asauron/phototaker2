975789ee09ff253061ab989be87cded0fa391235
asauron <asauron@gmail.com>
2012-12-15 00:33:41 -0800

    changed text to scrollable view

--- src/com/example/phototaker2/TakeTutorial.java
+++ src/com/example/phototaker2/TakeTutorial.java
@@ -1,6 +1,7 @@
 package com.example.phototaker2;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -19,7 +20,39 @@ public class TakeTutorial extends ListActivity {
 
   @Override
   protected void onListItemClick(ListView l, View v, int position, long id) {
-    String item = (String) getListAdapter().getItem(position);
+   // String item = (String) getListAdapter().getItem(position);
+	  String item = (String) getListAdapter().getItem(position);
+	    if (item.equals("Where to find the bees"))
+	    {
+	      Intent intent = new Intent(this, FirstPage.class);
+	      startActivity(intent);
+	      
+	    }
+	   else if (item.equals("Collecting Stranded Honey bees"))
+	    {
+		      Intent intent = new Intent(this, SecondPage.class);
+		      startActivity(intent);
+		      
+		    }
+	   else if (item.equals("Constructing a light trap"))
+	    {
+		      Intent intent = new Intent(this, ThirdPage.class);
+		      startActivity(intent);
+		      
+		    }
+	   else if (item.equals("Storing your samples"))
+	    {
+		      Intent intent = new Intent(this, FourthPage.class);
+		      startActivity(intent);
+		      
+		    }
+	   else if (item.equals("Upload your samples"))
+	    {
+		      Intent intent = new Intent(this, FifthPage.class);
+		      startActivity(intent);
+		      
+		    }
+    
     Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
   }
 } 
