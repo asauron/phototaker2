a22fb866c2573c5d61998b9f3211851cce7dea65
asauron <asauron@gmail.com>
2012-12-14 21:30:20 -0800

    Changed app icon and take tutorial data

--- src/com/example/phototaker2/TakeTutorial.java
+++ src/com/example/phototaker2/TakeTutorial.java
@@ -10,9 +10,8 @@ import android.widget.Toast;
 public class TakeTutorial extends ListActivity {
   public void onCreate(Bundle icicle) {
     super.onCreate(icicle);
-    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-        "Linux", "OS/2" };
+    String[] values = new String[] { "Where to find the bees", "Collecting Stranded Honey bees", "Constructing a light trap",
+        "Storing your samples", "Upload your samples"};
     ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
         android.R.layout.simple_list_item_1, values);
     setListAdapter(adapter);
